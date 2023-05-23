d972b111ebd5a968c1548e8634cd7ce543113903
Deepak Duggirala <ddeepak6992@gmail.com>
2023-05-23 15:20:08 -0400

    require queue to send task

--- sca_rhythm/__init__.py
+++ sca_rhythm/__init__.py
@@ -26,9 +26,15 @@ class NonRetryableException(Exception):
 def _validate_args(steps, name, app_id):
     assert len(steps) > 0, 'steps is empty'
     for i, step in enumerate(steps):
-        assert 'name' in step, f'step - {i} does not have "name" key'
-        assert len(step['name']) > 0, f'step - {i} name is empty'
-        assert 'task' in step, f'step - {i} does not have "task" key'
+        attrs = ['name', 'task']
+        for attr in attrs:
+            assert attr in step, f'step[{i}] does not have "{attr}" key'
+            assert isinstance(attr, str), f'step[{i}]["{attr}"] is not a string'
+            assert len(step[attr]) > 0, f'step[{i}]["{attr}"] is an empty string'
+        if 'queue' in step:
+            attr = 'queue'
+            assert isinstance(attr, str), f'step[{i}]["{attr}"] is not a string'
+            assert len(step[attr]) > 0, f'step[{i}]["{attr}"] is an empty string'
         # assert step['task'] in self.app.tasks, \
         #     f' step - {i} Task {step["task"]} is not registered in the celery application'
     names = [step['name'] for step in steps]
@@ -67,6 +73,16 @@ class Workflow:
             }
             self.wf_col.insert_one(self.workflow)
 
+    def wf_send_task(self, step: dict, task_args: list | tuple = None, task_kwargs: dict = None, **kwargs):
+        task_name = step['task']
+        task_queue = step.get('queue', None)
+
+        task_kwargs = task_kwargs or {}
+        task_kwargs['workflow_id'] = self.workflow['_id']
+        task_kwargs['step'] = step['name']
+
+        self.app.send_task(name=task_name, args=task_args, kwargs=task_kwargs, queue=task_queue, **kwargs)
+
     def start(self, *args, **kwargs):
         """
         Launches the task of the first step in this workflow.
@@ -77,13 +93,7 @@ class Workflow:
         :return: None
         """
         first_step = self.workflow['steps'][0]
-        # task = self.app.tasks[first_step['task']]
-
-        kwargs['workflow_id'] = self.workflow['_id']
-        kwargs['step'] = first_step['name']
-        # task.apply_async(args, kwargs)
-
-        self.app.send_task(first_step['task'], args, kwargs)
+        self.wf_send_task(first_step, args, kwargs)
 
     def pause(self):
         """
@@ -144,12 +154,7 @@ class Workflow:
                 assert not (task_inst is None and args is None), 'no args are provided and there is no last run task'
                 task_args = task_inst['args'] if task_inst is not None else args
 
-                kwargs = {
-                    'workflow_id': self.workflow['_id'],
-                    'step': step['name']
-                }
-                # task.apply_async(task_args, kwargs)
-                self.app.send_task(step['task'], task_args, kwargs)
+                self.wf_send_task(step, task_args)
                 # print(f' resuming step {step["name"]}')
                 return {
                     'resumed': True,
@@ -201,14 +206,7 @@ class Workflow:
 
         # apply next task with retval
         if next_step:
-            # next_task = self.app.tasks[next_step['task']]
-
-            kwargs = {
-                'workflow_id': self.workflow['_id'],
-                'step': next_step['name']
-            }
-            # next_task.apply_async((retval[0],), kwargs)
-            self.app.send_task(next_step['task'], (retval[0],), kwargs)
+            self.wf_send_task(next_step, (retval[0],))
             # print(f' starting next step {next_step["name"]}')
 
     def update(self):
